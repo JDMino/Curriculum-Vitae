264f924da27843d0a752c8aa5129022fa29571ce
JDMino <jdminogomez@gmail.com>
2023-02-25 13:50:22 -0300

    Versión Final

---
+++ script.js
@@ -0,0 +1,31 @@
+function mostrar() {
+    document.getElementById('detalles').style.display = 'block';
+}
+
+function ocultar() {
+    document.getElementById('detalles').style.display = 'none';
+}
+
+function mostrar2() {
+    document.getElementById('detalles-2').style.display = 'block';
+}
+
+function ocultar2() {
+    document.getElementById('detalles-2').style.display = 'none';
+}
+
+function mostrar3() {
+    document.getElementById('detalles-3').style.display = 'block';
+}
+
+function ocultar3() {
+    document.getElementById('detalles-3').style.display = 'none';
+}
+
+function mostrar4() {
+    document.getElementById('detalles-4').style.display = 'block';
+}
+
+function ocultar4() {
+    document.getElementById('detalles-4').style.display = 'none';
+}
